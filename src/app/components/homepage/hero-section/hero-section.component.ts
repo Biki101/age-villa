1648fab3f914fe9d65016529c1a39bf9062474ab
Biki101 <thakurbiki52@gmail.com>
2024-05-22 17:56:36 +0545

    updated herosection

--- src/app/components/homepage/hero-section/hero-section.component.ts
+++ src/app/components/homepage/hero-section/hero-section.component.ts
@@ -1,13 +1,29 @@
 import { Component } from '@angular/core';
 import { herSectionAssets } from '../../../utils';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-hero-section',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './hero-section.component.html',
   styleUrl: './hero-section.component.scss',
 })
 export class HeroSectionComponent {
   images = herSectionAssets;
+
+  currentImage = this.images[0]?.imageUrl;
+  currentIndex = 0;
+
+  constructor() {
+    setInterval(() => {
+      this.changeImage();
+    }, 5000);
+  }
+
+  changeImage() {
+    this.currentIndex = Math.floor(Math.random() * 3);
+    console.log(this.currentIndex);
+    this.currentImage = this.images[this.currentIndex]?.imageUrl;
+  }
 }
