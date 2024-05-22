2730bf5d330c7ecd4a451be28032a0decd8f29e9
Biki101 <thakurbiki52@gmail.com>
2024-05-22 16:39:49 +0545

    updated herosection

--- src/app/components/homepage/hero-section/hero-section.component.ts
+++ src/app/components/homepage/hero-section/hero-section.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
+import { herSectionAssets } from '../../../utils';
 
 @Component({
   selector: 'app-hero-section',
   standalone: true,
   imports: [],
   templateUrl: './hero-section.component.html',
-  styleUrl: './hero-section.component.scss'
+  styleUrl: './hero-section.component.scss',
 })
 export class HeroSectionComponent {
-
+  images = herSectionAssets;
 }
---
+++ src/app/utils.ts
@@ -0,0 +1,14 @@
+export const herSectionAssets = [
+  {
+    imageUrl:
+      'https://pixabay.com/get/g9d10ae70049579dd73fea6904621a845a24baf3e99dbd3f58703f7b81f3c10f0d7f02dbf9339477ab290a8213fbc5ae577c30ac42f5a3539f163138dffa8621e_1920.jpg',
+  },
+  {
+    imageUrl:
+      'https://pixabay.com/get/ge0b3122b6eb47b74bc6e3e4c4010e59849e734b22f83c5a8134664b4186a0caefa2f1eb2a0ad0db52a106f1e802ba936_1920.jpg',
+  },
+  {
+    imageUrl:
+      'https://pixabay.com/get/gd58af8779bf6cdfd7b3ba06807cb6a2a6d4f97b1e1eb3730b46c176efdb345e88616bc858b845f137d101e893971ec671312cc07c13625d7e313d54d23f6f565_1920.jpg',
+  },
+];
