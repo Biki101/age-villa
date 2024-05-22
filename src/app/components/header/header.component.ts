7ef0cd6d5dc772cdd64b3a1ce93a01ec0d76cb43
Biki101 <thakurbiki52@gmail.com>
2024-05-22 14:17:37 +0545

    updated header component

---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { faMap } from '@fortawesome/free-solid-svg-icons';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [FontAwesomeModule],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss',
+})
+export class HeaderComponent {
+  faEnvelope = faEnvelope;
+  faMap = faMap;
+}
