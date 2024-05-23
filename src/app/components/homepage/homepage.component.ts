265b2eeacc5c370b7383531a42f32c581c5403b5
Biki101 <thakurbiki52@gmail.com>
2024-05-23 10:36:28 +0545

    updated hero section

--- src/app/components/homepage/homepage.component.ts
+++ src/app/components/homepage/homepage.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeroSectionComponent } from './hero-section/hero-section.component';
+import { FeaturedSectionComponent } from './featured-section/featured-section.component';
 
 @Component({
   selector: 'app-homepage',
   standalone: true,
-  imports: [HeroSectionComponent],
+  imports: [HeroSectionComponent, FeaturedSectionComponent],
   templateUrl: './homepage.component.html',
   styleUrl: './homepage.component.scss',
 })
