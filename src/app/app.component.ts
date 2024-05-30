80e472dc92a016f16836f302e24638b5253daaf5
Biki101 <thakurbiki52@gmail.com>
2024-05-30 12:48:31 +0545

    initialized achievement

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,18 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HomepageComponent } from './components/homepage/homepage.component';
+import { AchievementSectionComponent } from './components/achievement-section/achievement-section.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent, HomepageComponent],
+  imports: [
+    RouterOutlet,
+    HeaderComponent,
+    FooterComponent,
+    HomepageComponent,
+    AchievementSectionComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
