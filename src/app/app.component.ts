88e87588eb5471261e1ae92f4f7c78eec73159d1
Biki101 <thakurbiki52@gmail.com>
2024-05-30 16:50:35 +0545

    added best deals

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,7 @@ import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HomepageComponent } from './components/homepage/homepage.component';
 import { AchievementSectionComponent } from './components/achievement-section/achievement-section.component';
+import { BestDealsComponent } from './components/best-deals/best-deals.component';
 
 @Component({
   selector: 'app-root',
@@ -14,6 +15,7 @@ import { AchievementSectionComponent } from './components/achievement-section/ac
     FooterComponent,
     HomepageComponent,
     AchievementSectionComponent,
+    BestDealsComponent,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
