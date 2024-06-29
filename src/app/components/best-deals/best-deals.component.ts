f1cb8da2f30b1ed9283618b23f84824e5053a80e
Biki101 <thakurbiki52@gmail.com>
2024-06-29 14:41:01 +0545

    finished best deal

--- src/app/components/best-deals/best-deals.component.ts
+++ src/app/components/best-deals/best-deals.component.ts
@@ -1,12 +1,39 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-best-deals',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './best-deals.component.html',
-  styleUrl: './best-deals.component.scss'
+  styleUrl: './best-deals.component.scss',
 })
 export class BestDealsComponent {
-
+  activeTab = 0;
+  details = [
+    {
+      space: 185,
+      floor: '26th',
+      rooms: 4,
+      parking: 'yes',
+      payment: 'Bank',
+      image: '../../../assets/best-deal1.jpg',
+    },
+    {
+      space: 250,
+      floor: '24th',
+      rooms: 5,
+      parking: 'yes',
+      payment: 'Bank',
+      image: '../../../assets/best-deal4.jpg',
+    },
+    {
+      space: 320,
+      floor: '34th',
+      rooms: 6,
+      parking: 'yes',
+      payment: 'Bank',
+      image: '../../../assets/best-deal3.jpg',
+    },
+  ];
 }
